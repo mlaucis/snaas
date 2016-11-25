2f18a702971e5186360b018af61043c3c46d1f7c
Alexander Simmerl <xla@tapglue.com>
2016-11-25 18:16:58 +0100

    Implement App creation

--- core/app.go
+++ core/app.go
@@ -1,13 +1,41 @@
 package core
 
 import (
+	"crypto/md5"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/tapglue/snaas/platform/generate"
 	"github.com/tapglue/snaas/service/app"
 )
+
+// AppCreateFunc creates a new App.
+type AppCreateFunc func(name, description string) (*app.App, error)
+
+// AppCreate creates a new App.
+func AppCreate(apps app.Service) AppCreateFunc {
+	return func(name, description string) (*app.App, error) {
+		token, backendToken, err := generateTokens()
+		if err != nil {
+			return nil, err
+		}
+
+		return apps.Put(app.NamespaceDefault, &app.App{
+			BackendToken: backendToken,
+			Description:  description,
+			Enabled:      true,
+			InProduction: false,
+			Name:         name,
+			Token:        token,
+		})
+	}
+}
 
 // AppFetchFunc returns the App for the given id.
 type AppFetchFunc func(id uint64) (*app.App, error)
 
-// AppFetchFunc returns the App for the given id.
+// AppFetch returns the App for the given id.
 func AppFetch(apps app.Service) AppFetchFunc {
 	return func(id uint64) (*app.App, error) {
 		as, err := apps.Query(app.NamespaceDefault, app.QueryOptions{
@@ -26,4 +54,27 @@ func AppFetch(apps app.Service) AppFetchFunc {
 
 		return as[0], nil
 	}
+}
+
+func generateTokens() (string, string, error) {
+	src := rand.NewSource(time.Now().UnixNano())
+
+	tokenHash := md5.New()
+	_, err := tokenHash.Write(generate.RandomBytes(src, 32))
+	if err != nil {
+		return "", "", err
+	}
+	token := fmt.Sprintf("%x", tokenHash.Sum(nil))
+
+	backendHash := md5.New()
+	_, err = backendHash.Write(generate.RandomBytes(src, 12))
+	if err != nil {
+		return "", "", err
+	}
+
+	return token, fmt.Sprintf(
+		"%s%s",
+		token,
+		fmt.Sprintf("%x", backendHash.Sum(nil))[:12],
+	), nil
 }
